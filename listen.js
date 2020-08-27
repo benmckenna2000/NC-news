6aec4a319789d483dd5463eea2b9735931f7f313
ben mckenna <benmckenna2000@outlook.com>
2020-08-27 14:37:41 +0100

    heroku changes

--- listen.js
+++ listen.js
@@ -1,6 +1,6 @@
 const { PORT = 9090 } = process.env;
 const express = require("express");
-const apiRouter = require("./routers/api.router");
+const apiRouter = require("./routers/apiRouter.js");
 const app = express();
 app.use("/api", apiRouter);
 app.use(express.json());
